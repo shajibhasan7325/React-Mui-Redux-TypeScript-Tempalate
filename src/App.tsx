c64c2d1f5ccaf0836592c08bb34c3740c9dd45e0
unknown <shajibhasan7325@gmail.com>
2023-03-29 16:13:41 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import Navbar from "./scenes/navbar";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { themeSettings } from "./theme";
+
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box height="100%" width="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>dashBord page</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
